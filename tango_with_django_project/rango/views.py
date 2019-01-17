dd73ba662a6e4d1f47ecd42ad310724e1dd4f258
Daniel Jiang <2433719J@student.gla.ac.uk>
2019-01-17 17:52:31 +1100

    As previous commit

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -8,6 +8,7 @@ def index(request):
 #    return HttpResponse("<a href = '/rango/about'> About </a>")
 
 def about(request):
-    return render(request, 'rango/about.html')
+    about_dict = {}
+    return render(request, 'rango/about.html', context=about_dict)
 #    return HttpResponse("Rango says here is the about page.\
 #    <br> <a href = '/rango'> Back home </a>")
