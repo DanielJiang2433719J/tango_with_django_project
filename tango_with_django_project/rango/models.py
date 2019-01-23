40034046f23d0040ce5c6986f1bde7bd69f0ca0e
Daniel Jiang <2433719J@student.gla.ac.uk>
2019-01-23 17:08:14 +1100

    everything little cooked, trying to delete db

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -3,6 +3,11 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique = True)
+    views = models.IntegerField(default = 0)
+    likes = models.IntegerField(default = 0)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
@@ -11,7 +16,7 @@ class Page(models.Model):
     category = models.ForeignKey(Category)
     title = models.CharField(max_length=128)
     url = models.URLField()
-    views = models.IntegerField(default=0)
+    views = models.IntegerField(default = 0)
 
     def __str__(self):
         return self.title
