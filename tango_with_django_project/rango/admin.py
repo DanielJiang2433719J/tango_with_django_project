88fb9808912c931a35f3c47ab58e09d84f77c358
Daniel Jiang <2433719J@student.gla.ac.uk>
2019-01-24 23:36:50 +1100

    Added testing files and fixed whitespacing for tests

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-admin.site.register(Category)
-
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
+# Customises admin interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
 admin.site.register(Page, PageAdmin)
+
+# Update registration to include customised interface
+admin.site.register(Category, CategoryAdmin)
